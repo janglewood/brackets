08982614bfc2058c9b0b7f085da07b9a205e2fdc
Anton Karatkevich <zhl96@mail.ru>
2019-02-22 21:28:16 +0300

    Task brackets done with mistakes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig.map(function (item, i) {
+      let open = item[0];
+      let close = item[1];
+      let newstr;
+
+      while(str.includes(open) && str.includes(close)) {
+        str = str.replace(open, '');
+        str = str.replace(close, '');
+      }
+  });
+  return str ?  false : true;
 }
