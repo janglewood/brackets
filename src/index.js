32c64adf242e3fe2735c7edfb174b165daacbf12
Anton Karatkevich <zhl96@mail.ru>
2019-03-03 15:39:14 +0300

    Add right solution

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  bracketsConfig.map(function (item, i) {
-      let open = item[0];
-      let close = item[1];
-      let newstr;
-
-      while(str.includes(open) && str.includes(close)) {
-        str = str.replace(open, '');
-        str = str.replace(close, '');
+  let stack = [];
+  if (str.length % 2 !== 0) return false;
+      for (let i = 0; i < str.length; i++) {
+          for (let k = 0; k < bracketsConfig.length; k++) {
+              if (str[i] === bracketsConfig[k][1] && stack[stack.length - 1] === bracketsConfig[k][0]) {
+                  stack.pop();
+              } else if (str[i] === bracketsConfig[k][0]) {
+                  stack.push(str[i]);
+              }
+          }
       }
-  });
-  return str ?  false : true;
+      return stack.length === 0;
 }
